b57a89c5241b0a2eadd1ede6c94b23a9a22d00cc
Maxwell Weru <mburumaxwell@tingle.software>
2020-01-22 11:25:17 +0300

    added code comments for authorization providers

--- Sources/TingleApiClient/AuthenticationHeaderProvider.swift
+++ Sources/TingleApiClient/AuthenticationHeaderProvider.swift
@@ -7,15 +7,31 @@
 
 import Foundation
 
+/**
+ * Implementation of `IAuthenticationProvider` which sets the Authorization header using the scheme and parameter separated by a space.
+ * The parameter set is gotten from the abstract method `getParameter(request: inout URLRequest)`.
+ */
 public class AuthenticationHeaderProvider: IAuthenticationProvider {
     private static let DEFAULT_SCHEME = "Bearer"
     
+    /**
+     * The scheme to be set in the `Authorization` header
+     */
     public let scheme: String
     
+    /**
+     * Initializes an instance of `AuthenticationHeaderProvider`
+     * - Parameter scheme: The scheme to set in the `Authorization` header
+     */
     init(_ scheme: String = DEFAULT_SCHEME) {
         self.scheme = scheme
     }
     
+    /**
+     * Process a request before sending
+     *
+     * - Parameter request: The request that needs to be processed before sending
+     */
     public func process(request: inout URLRequest) -> URLRequest {
         let parameter = getParameter(request: &request)
         let headerValue = "\(scheme) \(parameter)"
@@ -23,10 +39,22 @@ public class AuthenticationHeaderProvider: IAuthenticationProvider {
         return request
     }
     
+    /**
+     * Gets the paramter that should be included in the `Authorization` header
+     *
+     * - Parameter request: The request that needs to be processed before sending
+     */
     open func getParameter(request: inout URLRequest) -> String {
         fatalError("getParameter must be implemented")
     }
     
+    /**
+     * Process a response received
+     *
+     * - Parameter response: The response that needs to be processed
+     * - Parameter data: The data in the response
+     * - Parameter error: An error processing the request
+     */
     public func process(response: URLResponse?, data: Data?, error: Error?) {
         
     }
