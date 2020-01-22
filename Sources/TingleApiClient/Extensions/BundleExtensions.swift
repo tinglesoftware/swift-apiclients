66936cf7025e5cbd9081fe2faabd3e83e25a6f03
Maxwell Weru <mburumaxwell@tingle.software>
2020-01-22 10:59:59 +0300

    added comments to BundleExtensions

--- Sources/TingleApiClient/Extensions/BundleExtensions.swift
+++ Sources/TingleApiClient/Extensions/BundleExtensions.swift
@@ -8,9 +8,21 @@
 import Foundation
 
 extension Bundle {
+    
+    /**
+     * The receiver's short version. Usually done in semantic versioning format
+     *
+     * The bundle identifier is defined by the `CFBundleShortVersionString` key in the bundle’s information property list.
+     */
     var shortBundleVersion: String? {
         return infoDictionary?["CFBundleShortVersionString"] as? String
     }
+    
+    /**
+     * The receiver's bundle version. Usually an incrementing integer.
+     *
+     * The bundle identifier is defined by the `CFBundleVersion` key in the bundle’s information property list.
+    */
     var bundleVersion: String? {
         return infoDictionary?["CFBundleVersion"] as? String
     }
