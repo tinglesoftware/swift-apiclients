ab8c40f6dca1138c66c88517f9c1562b922f6c97
Maxwell Weru <mburumaxwell@tingle.software>
2020-06-06 08:53:34 +0300

    raise minimum iOS version to v11 since it is expected that the usage of this package will be in environments/products targeting "current-1" scheme (i.e. iOS 12+). Keeping iOS v11 and not v12 gives apps that are not yet migrated some more time

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TingleApiClient",
-    platforms: [ .iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3) ],
+    platforms: [ .iOS(.v11), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3) ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
