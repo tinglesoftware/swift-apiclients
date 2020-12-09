69284dbd322a4137bdf141fe0e8bbf2dac12eee3
Seth Onyango <seth.onyango@tingle.software>
2020-12-09 03:34:30 +0300

    Use logging framework to print error messages

--- Sources/TingleApiClient/TingleApiClient.swift
+++ Sources/TingleApiClient/TingleApiClient.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Logging
 
 /**
  * Middleware for performing authentication
@@ -168,6 +169,8 @@ open class TingleApiClient {
                         problem = try! self.decoder.decode(HttpApiResponseProblem.self, from: data!)
 
                     default:
+                        let logger = Logger(label: String(describing: TingleApiClient.self))
+                        logger.error("")
                         print("Response Status Code \(statusCode)")
                     }
                 }
