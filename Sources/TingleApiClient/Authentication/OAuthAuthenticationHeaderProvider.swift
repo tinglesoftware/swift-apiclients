f33f5736eecdea3d34100a18d35bdb6b302d4f91
Seth Onyango <seth.onyango@tingle.software>
2020-12-13 15:39:17 +0300

    Add initialisers for instances of OAuthAuthenticationHeaderProvider

--- Sources/TingleApiClient/Authentication/OAuthAuthenticationHeaderProvider.swift
+++ Sources/TingleApiClient/Authentication/OAuthAuthenticationHeaderProvider.swift
@@ -1,3 +1,67 @@
+//
+//  OAuthAuthenticationHeaderProvider.swift
+//  
+//
+//  Created by Seth Onyango on 13/12/2020.
+//
+
+import Foundation
+/**
+ * Authentication provider for OAuth Client Credentials flow (see the OAuth 2.0 spec for more details).
+ * This provider supports caching
+ */
+public final class OAuthAuthenticationHeaderProvider: AuthenticationHeaderProvider{
+    private static let DEFAULT_SCHEME = "Bearer"
+    
+    private var oAuthRequest: OAuthRequest
+    
+    /**
+     * Initializes an instance of `OAuthAuthenticationHeaderProvider`
+     * - Parameter scheme: The scheme to set in the `Authorization` header
+     * - Parameter oAuthRequest: The request model to be used to request a token. Contains parameters like clientId, clientSecret e.t.c
+     */
+    public init(_ scheme: String, oAuthRequest: OAuthRequest){
+        self.oAuthRequest = oAuthRequest
+        super.init(scheme)
+    }
+   
+    /**
+     * Initializes an instance of `OAuthAuthenticationHeaderProvider`
+     * - Parameter oAuthRequest: The request model to be used to request a token. Contains parameters like clientId, clientSecret e.t.c
+     */
+    public convenience init(oAuthRequest: OAuthRequest){
+        self.init(OAuthAuthenticationHeaderProvider.DEFAULT_SCHEME, oAuthRequest: oAuthRequest)
+    }
+    
+   
+    /**
+     * Initializes an instance of `OAuthAuthenticationHeaderProvider`
+     * - Parameter authenticationEndpoint The authentication endpoint to be used to request a token
+     * - Parameter clientId The client identifier (client_id) to be used in the token request
+     * - Parameter clientSecret The client secret (client_secret) to be used in the token request
+     * - Parameter resource The resource to be requested for in the token request
+     */
+    public convenience init(authenticationEndpoint: String?, clientId: String?,clientSecret: String?, resource: String?) {
+        let oAuthRequest = OAuthRequest(authenticationEndpoint: authenticationEndpoint, clientId: clientId, clientSecret: clientSecret, resource: resource)
+        self.init(oAuthRequest: oAuthRequest)
+    }
+    
+    /**
+     * Initializes an instance of `OAuthAuthenticationHeaderProvider`
+     * - Parameter scheme The scheme to set in the `Authorization` header
+     * - Parameter authenticationEndpoint The authentication endpoint to be used to request a token
+     * - Parameter clientId The client identifier (client_id) to be used in the token request
+     * - Parameter clientSecret The client secret (client_secret) to be used in the token request
+     * - Parameter resource The resource to be requested for in the token request
+     */
+    public convenience init(_ scheme: String, authenticationEndpoint: String?, clientId: String?,clientSecret: String?, resource: String?) {
+        let oAuthRequest = OAuthRequest(authenticationEndpoint: authenticationEndpoint, clientId: clientId, clientSecret: clientSecret, resource: resource)
+        self.init(scheme,oAuthRequest: oAuthRequest)
+    }
+    
+}
+
+
 public class OAuthRequest{
     //  the authentication endpoint to be used to request a token
     var authenticationEndpoint: String? = nil
