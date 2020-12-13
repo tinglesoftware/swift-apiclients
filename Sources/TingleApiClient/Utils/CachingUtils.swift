50a36a7dad61d9ae0ff70eebef30f8d2516be309
Seth Onyango <seth.onyango@tingle.software>
2020-12-13 12:14:35 +0300

    Store user's token in UserDefaults

--- Sources/TingleApiClient/Utils/CachingUtils.swift
+++ Sources/TingleApiClient/Utils/CachingUtils.swift
@@ -1,8 +1,22 @@
 //
-//  File.swift
+//  CachingUtils.swift
 //  
 //
 //  Created by Seth Onyango on 13/12/2020.
 //
 
 import Foundation
+
+struct CachingUtils {
+    private static let PREF_ACCESS_TOKEN = "access_token_"
+    
+    private static let defaults: UserDefaults = UserDefaults.standard
+
+    static var accessToken: String? {
+        get {
+            defaults.string(forKey: PREF_ACCESS_TOKEN)
+        } set {
+            defaults.set(newValue, forKey: PREF_ACCESS_TOKEN)
+        }
+    }
+}
