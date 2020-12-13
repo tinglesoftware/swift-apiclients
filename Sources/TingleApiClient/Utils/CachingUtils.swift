2278319367cd0a61e1c72971c54e411483c0cdeb
Seth Onyango <seth.onyango@tingle.software>
2020-12-13 12:17:33 +0300

    Check of the token is expired

--- Sources/TingleApiClient/Utils/CachingUtils.swift
+++ Sources/TingleApiClient/Utils/CachingUtils.swift
@@ -9,7 +9,8 @@ import Foundation
 
 struct CachingUtils {
     private static let PREF_ACCESS_TOKEN = "access_token_"
-    
+    private static let PREF_ACCESS_TOKEN_EXPIRY = "access_token_expiry_"
+
     private static let defaults: UserDefaults = UserDefaults.standard
 
     static var accessToken: String? {
@@ -18,5 +19,18 @@ struct CachingUtils {
         } set {
             defaults.set(newValue, forKey: PREF_ACCESS_TOKEN)
         }
+    }
+    
+    static var accessTokenExpiry: CLong {
+        get{
+            defaults.integer(forKey: PREF_ACCESS_TOKEN_EXPIRY)
+        } set {
+            defaults.set(newValue, forKey: PREF_ACCESS_TOKEN_EXPIRY)
+        }
+    }
+    
+    static var hasTokenExpired: Bool {
+        let currentTime_ms = Int(Date().timeIntervalSinceNow * 1000)
+        return currentTime_ms >= accessTokenExpiry
     }
 }
