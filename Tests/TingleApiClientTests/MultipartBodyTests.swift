91b0ddf1bea8d111b233d692d1ef2c82b250d2ef
Seth Onyango <sethi.onyango@gmail.com>
2021-09-08 07:51:40 +0300

    Update form data tests

--- Tests/TingleApiClientTests/MultipartBodyTests.swift
+++ Tests/TingleApiClientTests/MultipartBodyTests.swift
@@ -5,11 +5,9 @@ class MutlipartBodyTests: XCTestCase{
     func testSerializationOfFormData(){
         let url = URL(string: "www.example.com")!
         var request = URLRequest(url: url)
-        request.setValue("multipart/form-data;", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
 
-        let body = try! MultipartBody.Builder(request)
-            .setType(type: .FORM)
+        let body = try! MultipartBody.Builder(&request, type: .FORM)
             .addFormDataPart(name: "Currecny", value:"USD")
             .addFormDataPart(name: "Amount", value: "1234")
             .build()
